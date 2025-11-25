1ea74a71d7a3690f2259be06ebbf2ce10214243e
ines-charfi <ines.charfi@laplateforme.io>
2025-11-25 11:50:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# safetynetV1
--- src/main/java/com/ines/safetynet1/service/dto/FireStationCoverageDto.java
+++ src/main/java/com/ines/safetynet1/service/dto/FireStationCoverageDto.java
@@ -1,4 +1,34 @@
 package com.ines.safetynet1.service.dto;
 
+import java.util.List;
+
 public class FireStationCoverageDto {
+    private List<PersonInfoDto> residents;
+    private int adults;
+    private int children;
+
+    public int getAdults() {
+        return adults;
+    }
+
+    public void setAdults(int adults) {
+        this.adults = adults;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public List<PersonInfoDto> getResidents() {
+        return residents;
+    }
+
+    public void setResidents(List<FireStationPersonDto> residents) {
+        this.residents = residents;
+    }
 }
+
